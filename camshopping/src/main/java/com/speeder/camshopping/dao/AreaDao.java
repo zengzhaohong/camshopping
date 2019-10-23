d2cbebce49aa5a6c3d3d49fa408c11584678b918
zengzhaohong <1115827740@qq.com>
2019-10-23 11:24:44 +0800

    delete camshopping

--- camshopping/src/main/java/com/speeder/camshopping/dao/AreaDao.java
+++
@@ -1,9 +0,0 @@
-package com.speeder.camshopping.dao;
-
-import java.util.List;
-import com.speeder.camshopping.entity.Area;
-
-public interface AreaDao {
-
-	List<Area> queryArea();
-}
--- camshopping/src/main/java/com/speeder/camshopping/service/AreaService.java
+++
@@ -1,9 +0,0 @@
-package com.speeder.camshopping.service;
-
-import java.util.List;
-
-import com.speeder.camshopping.entity.Area;
-
-public interface AreaService {
-	List<Area> getAreaList();
-}
